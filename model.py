58b3e3b49260fcaa73937b4c6f6493c83671f6a0
davidlainesv <david.dev@outlook.com>
2023-03-17 04:41:56 -0600

    ok

--- model.py
+++ model.py
@@ -35,10 +35,10 @@ def focal_loss(gamma=2., alpha=4.):
         y_true = tf.convert_to_tensor(y_true, tf.float32)
         y_pred = tf.convert_to_tensor(y_pred, tf.float32)
 
-        model_out = tf.add(y_pred, epsilon)
-        ce = tf.multiply(y_true, -tf.log(model_out))
-        weight = tf.multiply(y_true, tf.pow(tf.subtract(1., model_out), gamma))
-        fl = tf.multiply(alpha, tf.multiply(weight, ce))
+        model_out = tf.math.add(y_pred, epsilon)
+        ce = tf.math.multiply(y_true, -tf.math.log(model_out))
+        weight = tf.math.multiply(y_true, tf.math.pow(tf.math.subtract(1., model_out), gamma))
+        fl = tf.math.multiply(alpha, tf.math.multiply(weight, ce))
         reduced_fl = tf.reduce_max(fl, axis=1)
         return tf.reduce_mean(reduced_fl)
     return focal_loss_fixed
