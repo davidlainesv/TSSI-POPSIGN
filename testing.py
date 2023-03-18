8f8dece5b83cb9fd66a74aaf1e47dec4fc070c15
davidlainesv <david.dev@outlook.com>
2023-03-17 23:05:26 -0600

    testing available

--- testing.py
+++ testing.py
@@ -5,9 +5,8 @@ import numpy as np
 import wandb
 from wandb.keras import WandbCallback, WandbModelCheckpoint
 import tensorflow as tf
-import pandas as pd
-from model import build_densenet121_model, build_efficientnet_model, build_mobilenetv2_model
-from optimizer import build_sgd_optimizer
+from model import build_densenet121_model, build_efficientnet_model
+from optimizer import build_adam_optimizer, build_sgd_optimizer
 from utils import str2bool
 
 dataset = None
@@ -46,15 +45,21 @@ def run_experiment(config=None, log_to_wandb=True, verbose=0):
     print("[INFO] Dataset Total examples:", dataset.num_total_examples)
     print("[INFO] Dataset Training examples:", dataset.num_train_examples
           + dataset.num_val_examples)
-    print("[INFO] Dataset Testing examples:", dataset.num_test_examples)
 
     # setup optimizer
-    optimizer = build_sgd_optimizer(initial_learning_rate=config['initial_learning_rate'],
-                                    maximal_learning_rate=config['maximal_learning_rate'],
-                                    momentum=config['momentum'],
-                                    nesterov=config['nesterov'],
-                                    step_size=config['step_size'],
-                                    weight_decay=config['weight_decay'])
+    if config["optimizer"] == "adam":
+        optimizer = build_sgd_optimizer(initial_learning_rate=config['initial_learning_rate'],
+                                        maximal_learning_rate=config['maximal_learning_rate'],
+                                        momentum=config['momentum'],
+                                        nesterov=config['nesterov'],
+                                        step_size=config['step_size'],
+                                        weight_decay=config['weight_decay'])
+    elif config["optimizer"] == "adam":
+        optimizer = build_adam_optimizer(initial_learning_rate=config['initial_learning_rate'],
+                                         maximal_learning_rate=config['maximal_learning_rate'],
+                                         step_size=config['step_size'],
+                                         weight_decay=config['weight_decay'],
+                                         epsilon=config['epsilon'])
 
     # setup model
     input_shape = [None, dataset.input_width, 3]
@@ -63,18 +68,13 @@ def run_experiment(config=None, log_to_wandb=True, verbose=0):
                                            dropout=config['dropout'],
                                            optimizer=optimizer,
                                            pretraining=config['pretraining'])
-    elif config['backbone'] == "mobilenet":
-        model = build_mobilenetv2_model(input_shape=input_shape,
-                                        dropout=config['dropout'],
-                                        optimizer=optimizer,
-                                        pretraining=config['pretraining'])
     elif config['backbone'] == "efficientnet":
         model = build_efficientnet_model(input_shape=input_shape,
                                          dropout=config['dropout'],
                                          optimizer=optimizer,
                                          pretraining=config['pretraining'])
     else:
-        raise Exception("Unknown model name")
+        raise Exception("Model unknown")
 
     print("[INFO] Input Shape:", input_shape)
 
@@ -102,29 +102,6 @@ def run_experiment(config=None, log_to_wandb=True, verbose=0):
               validation_data=validation_dataset,
               callbacks=callbacks)
 
-    # extra_epochs = 10
-    # steps_per_epoch = np.ceil(
-    #     dataset.num_train_examples / config["batch_size"])
-    # new_step_size = extra_epochs * steps_per_epoch
-    # metrics = [tf.keras.metrics.TopKCategoricalAccuracy(
-    #     k=1, name='top_1', dtype=tf.float32)]
-    # optimizer = build_sgd_optimizer(initial_learning_rate=config['initial_learning_rate'],
-    #                                 maximal_learning_rate=config['initial_learning_rate'] / 10,
-    #                                 momentum=config['momentum'],
-    #                                 nesterov=config['nesterov'],
-    #                                 step_size=new_step_size,
-    #                                 weight_decay=config['weight_decay'])
-    # model.compile(
-    #     optimizer=optimizer,
-    #     loss='categorical_crossentropy',
-    #     metrics=metrics,
-    # )
-    # model.fit(train_dataset,
-    #           epochs=extra_epochs,
-    #           verbose=verbose,
-    #           validation_data=validation_dataset,
-    #           callbacks=callbacks)
-
     # get the logs of the model
     return model.history
 
@@ -151,6 +128,9 @@ def main(args):
     batch_size = args.batch_size
     num_epochs = args.num_epochs
     pipeline = args.pipeline
+    optimizer = args.optimizer
+    epsilon = args.epsilon
+    focal_loss = args.focal_loss
     save_freq = args.save_freq or num_epochs
 
     dataset = Dataset(concat_validation_to_train=True)
@@ -174,6 +154,9 @@ def main(args):
         'batch_size': batch_size,
         'pipeline': pipeline,
 
+        'optimizer': optimizer,
+        'epsilon': epsilon,
+        'focal_loss': focal_loss,
         'save_freq': int(steps_per_epoch * save_freq)
     }
 
@@ -193,6 +176,10 @@ if __name__ == "__main__":
                         help='Add pretraining', default=True)
     parser.add_argument('--augmentation', type=str2bool,
                         help='Add augmentation', default=False)
+    parser.add_argument('--focal_loss', type=str2bool,
+                        help='Use focal loss', default=False)
+    parser.add_argument('--optimizer', type=str,
+                        help='Optimizer: \'sgd\', \'adam\'', default='sgd')
     parser.add_argument('--lr_min', type=float,
                         help='Minimum learning rate', default=0.0001)
     parser.add_argument('--lr_max', type=float,
@@ -201,6 +188,8 @@ if __name__ == "__main__":
                         help='Minimum learning rate', default=0.3)
     parser.add_argument('--weight_decay', type=float,
                         help='Minimum learning rate', default=1e-7)
+    parser.add_argument('--epsilon', type=float,
+                        help='Epsilon (for adam optimizer)', default=1e-7)
     parser.add_argument('--batch_size', type=int,
                         help='Batch size of training and testing', default=32)
     parser.add_argument('--num_epochs', type=int,
