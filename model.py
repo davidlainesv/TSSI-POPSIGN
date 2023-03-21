e9a683a0140bea57545252adef9c57f95d49a474
davidlainesv <david.dev@outlook.com>
2023-03-21 00:34:23 -0600

    num_channels = 2

--- model.py
+++ model.py
@@ -46,59 +46,12 @@ def focal_loss(gamma=2., alpha=4.):
     return focal_loss_fixed
 
 
-def build_densenet121_model_legacy(input_shape=[None, 128, 3], dropout=0,
-                                   optimizer=None, pretraining=True):
-    # setup model
-    base_model = None
-    if pretraining:
-        # path_to_downloaded_file = tf.keras.utils.get_file(
-        #     "tssi_densenet_wlasl100.zip",
-        #     "https://storage.googleapis.com/cloud-ai-platform-f3305919-42dc-47f1-82cf-4f1a3202db74/tssi_densenet_wlasl100.zip",
-        #     extract=True)
-        # path_to_downloaded_file = path_to_downloaded_file.replace(".zip", "")
-
-        # inputs = Input(shape=input_shape)
-        # x = DenseNet121(input_shape=input_shape, weights=None,
-        #                 include_top=False, pooling='avg')(inputs)
-        # x = Dropout(0)(x)
-        # base_model = Model(inputs=inputs, outputs=x)
-        # base_model.load_weights(path_to_downloaded_file + "/weights")
-        # # base_model.trainable = False
-        # x = base_model(inputs, training=False)
-        # x = Dropout(dropout)(x)
-        # predictions = Dense(NUM_CLASSES, activation='softmax')(x)
-        # model = Model(inputs=base_model.input, outputs=predictions)
-        inputs = Input(shape=input_shape)
-        x = DenseNet121(input_shape=input_shape, weights="imagenet",
-                        include_top=False, pooling='avg')(inputs)
-        x = Dropout(dropout)(x)
-        predictions = Dense(NUM_CLASSES, activation='softmax')(x)
-        model = Model(inputs=inputs, outputs=predictions)
-    else:
-        inputs = Input(shape=input_shape)
-        x = DenseNet121(input_shape=input_shape, weights=None,
-                        include_top=False, pooling='avg')(inputs)
-        x = Dropout(dropout)(x)
-        predictions = Dense(NUM_CLASSES, activation='softmax')(x)
-        model = Model(inputs=inputs, outputs=predictions)
-
-    # setup the metrics
-    metrics = [
-        TopKCategoricalAccuracy(k=1, name='top_1', dtype=tf.float32)
-    ]
-
-    # compile the model
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy',
-                  metrics=metrics)
-
-    return model, base_model
-
-
-def build_densenet121_model(input_shape=[None, 128, 3], dropout=0,
+def build_densenet121_model(input_shape=[None, 135, 2], dropout=0,
                             optimizer=None, pretraining=True, use_focal_loss=False):
     # setup model
     weights = 'imagenet' if pretraining else None
     inputs = Input(shape=input_shape)
+    inputs = inputs[:, :, :, 2]
     x = DenseNet121(input_shape=input_shape, weights=weights,
                     include_top=False, pooling='avg')(inputs)
     x = Dropout(dropout)(x)
